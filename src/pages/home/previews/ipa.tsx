5b4d5704e2c50dd9f032b95729a89ba2b07d71ce
SweetHome <103113557+li-peifeng@users.noreply.github.com>
2024-12-25 08:30:49 +0100

    更新 ipa.tsx

--- src/pages/home/previews/ipa.tsx
+++ src/pages/home/previews/ipa.tsx
@@ -1,10 +1,10 @@
 import { Button, HStack } from "@hope-ui/solid"
 import { createSignal } from "solid-js"
-import { useT, useLink } from "~/hooks"
+import { useT, useLink, useCopyLink
+} from "~/hooks"
 import { objStore } from "~/store"
 import { api, baseName, safeBtoa } from "~/utils"
 import { FileInfo } from "./info"
-import { Download } from "./download"
 
 const Ipa = () => {
   const t = useT()
@@ -15,14 +15,9 @@ const Ipa = () => {
   return (
     <FileInfo>
       <HStack spacing="$2">
-        <Button as="a" href={objStore.raw_url} target="_blank">
-          {t("home.preview.download")}
-        </Button>
-        <Button colorScheme="accent" onClick={() => copyCurrentRawLink(true)}>
-          {t("home.toolbar.copy_link")}
-        </Button>
         <Button
           as="a"
+          colorScheme="accent"
           href={
             "itms-services://?action=download-manifest&url=" +
             `${api}/i/${safeBtoa(
@@ -39,7 +34,7 @@ const Ipa = () => {
         </Button>
         <Button
           as="a"
-          colorScheme="primary"
+          colorScheme="warning"
           href={
             "apple-magnifier://install?url=" +
             encodeURIComponent(currentObjLink(true))
@@ -49,6 +44,12 @@ const Ipa = () => {
           }}
         >
           {t(`home.preview.${trInstalling() ? "tr-installing" : "tr-install"}`)}
+        </Button>
+        <Button colorScheme="success" onClick={() => copyCurrentRawLink(true)}>
+          {t("home.toolbar.copy_link")}
+        </Button>
+         <Button as="a" colorScheme="primary" href={objStore.raw_url} target="_blank">
+          {t("home.preview.download")}
         </Button>
       </HStack>
     </FileInfo>
